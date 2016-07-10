814bc8d686d5f8e5eab9947f671d73bb4c37d58a
jackscotti <jacopojackscotti@gmail.com>
2016-07-10 15:11:08 +0100

    Make start and end compulsory

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -2,18 +2,26 @@ require 'rails_helper'
 
 describe Booking do
   context "Creating a booking" do
-    context "validates the presence of a room" do
+    context "validates the presence of compulsory fields" do
       let(:booking) { Booking.new }
-      let(:room) { Room.new }
 
-      it "errors when not assigned to a room" do
+      it "cannot be created when compulsory parameters are not provided" do
         booking.save
-        expect(booking.errors.messages).to include(:room => ["can't be blank"])
+        expect(booking.errors.messages).to eq(
+          :room => ["can't be blank"],
+          :start=>["can't be blank"],
+          :end=>["can't be blank"]
+        )
       end
+
       it "creates the booking when assigned to an existing room" do
+        booking.start = DateTime.now + 1.day - 1.hour
+        booking.end = DateTime.now + 1.day + 1.hour
+        room = Room.new
         booking.room = room
         booking.save
-        expect(booking.errors.messages).not_to include(:room => ["can't be blank"])
+
+        expect(booking.errors.messages).to be_empty
       end
     end
   end
