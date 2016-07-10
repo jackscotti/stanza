5c03df0a25edb36b4f2fa36822508d2ae0029175
jackscotti <jacopojackscotti@gmail.com>
2016-07-10 20:03:46 +0100

    Refactor booking model and its test
    Remove unnecessary references to self and refactor tests

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,13 +6,11 @@ private
 
   def validate_availability
     # should not check for past bookings, this can be vastly optimised with an appropriate query
-    if self.room
-      self.room.bookings.each do |b|
-        next if self == b
+    room.bookings.each do |b|
+      next if self == b
 
-        if b.start_time <= self.start_time && b.end_time >= self.end_time
-          errors.add(:room_availability, "the room is already booked")
-        end
+      if b.start_time <= start_time && b.end_time >= end_time
+        errors.add(:room_availability, "the room is already booked")
       end
     end
   end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 describe Booking do
+  before :each do
+    @room = Room.new
+    @room.save
+  end
+
   context "Creating a booking" do
     context "validates the presence of compulsory fields" do
-      let(:booking) { Booking.new }
-
       it "creates the booking when assigned to an existing room" do
-        booking.start_time = DateTime.now + 1.day - 1.hour
-        booking.end_time = DateTime.now + 1.day + 1.hour
-        room = Room.new
-        booking.room = room
+        booking = Booking.new(
+          start_time: DateTime.now + 1.day - 1.hour,
+          end_time: DateTime.now + 1.day + 1.hour,
+          room: @room,
+        )
         booking.save
 
         expect(booking.errors.messages).to be_empty
@@ -18,15 +22,15 @@ describe Booking do
 
     context "validates the availability of a room" do
       before do
-        @room = Room.new
         @booking_1 = Booking.new(
           start_time: DateTime.now + 1.day - 1.hour,
           end_time: DateTime.now + 1.day + 1.hour,
           room: @room,
         )
+
+        @booking_1.save
         @room.bookings << @booking_1
         @room.save
-        @booking_1.save
       end
 
       it "allows bookings when a room is available" do
@@ -59,12 +63,10 @@ describe Booking do
     context "checks whether the date is valid" do
       it "does not allow an end date that is earlier than the start_time date" do
         booking = Booking.new
-        room = Room.new
-        room.save
 
         booking.start_time = DateTime.now + 1.day + 1.hour
         booking.end_time = DateTime.now + 1.day - 1.hour
-        booking.room = room
+        booking.room = @room
 
         booking.save
 
