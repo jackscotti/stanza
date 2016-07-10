f8b0b743ba4dfaa5839fe455139be4923f2e3b54
jackscotti <jacopojackscotti@gmail.com>
2016-07-10 08:39:08 +0100

    Add Room and Booking
    Room has_many Bookings
    Booking belongs_to Room

---
+++ db/migrate/20160710071919_create_rooms.rb
@@ -0,0 +1,12 @@
+class CreateRooms < ActiveRecord::Migration[5.0]
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.string :description
+      t.string :location
+      t.integer :capacity
+
+      t.timestamps
+    end
+  end
+end
