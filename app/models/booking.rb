59cbf995fd123ac711fb429098f203eae518e5de
jackscotti <jacopojackscotti@gmail.com>
2016-07-10 15:36:15 +0100

    Validate whether a room is booked already

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,20 @@
 class Booking < ApplicationRecord
   belongs_to :room
   validates :room, :start, :end, :presence => true
+  validate :validate_availability
+
+private
+
+  def validate_availability
+    # should not check for past bookings, this can be vastly optimised with an appropriate query
+    if self.room
+      self.room.bookings.each do |b|
+        next if self == b
+
+        if b.start <= self.start && b.end >= self.end
+          errors.add(:room_availability, "the room is already booked")
+        end
+      end
+    end
+  end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -9,8 +9,8 @@ describe Booking do
         booking.save
         expect(booking.errors.messages).to eq(
           :room => ["can't be blank"],
-          :start=>["can't be blank"],
-          :end=>["can't be blank"]
+          :start =>["can't be blank"],
+          :end=>["can't be blank"],
         )
       end
 
@@ -22,6 +22,47 @@ describe Booking do
         booking.save
 
         expect(booking.errors.messages).to be_empty
+      end
+    end
+
+    context "validates the availability of a room" do
+      let(:room) { Room.new }
+      let(:booking_1) { Booking.new(
+        start: DateTime.now + 1.day - 1.hour,
+        end: DateTime.now + 1.day + 1.hour,
+        room: room,
+      )}
+
+      before do
+        room.bookings << booking_1
+        room.save
+        booking_1.save
+      end
+
+      it "allows bookings when a room is available" do
+        booking_2 = Booking.new(
+          start: DateTime.now + 2.day - 1.hour,
+          end: DateTime.now + 2.day + 1.hour,
+          room: room,
+        )
+
+        booking_2.save
+
+        expect(booking_2.errors.messages).to be_empty
+      end
+
+      it "does not allow bookings when a room is not available" do
+        booking_2 = Booking.new(
+          start: DateTime.now + 1.day - 30.minutes,
+          end: DateTime.now + 1.day + 30.minutes,
+          room: room,
+        )
+
+        booking_2.save
+
+        expect(booking_2.errors.messages).to eq(
+          :room_availability => ["the room is already booked"]
+        )
       end
     end
   end
